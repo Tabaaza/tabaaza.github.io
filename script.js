cd6ed3d6b4998030dd5775d745b4cc5dc4983797
Tabaaza <omar.tabaza@gmail.com>
2024-06-02 11:40:41 +0200

    Change phone size

--- script.js
+++ script.js
@@ -2,11 +2,7 @@ function generateLink() {
   const countryCode = document.getElementById('country-code').value;
   let phone = document.getElementById('phone').value;
   const message = document.getElementById('message').value;
-
-  // Remove any spaces or non-numeric characters from phone number
   phone = phone.replace(/\D/g, '');
-
   const whatsappLink = `https://wa.me/${countryCode}${phone}?text=${encodeURIComponent(message)}`;
-  
   window.open(whatsappLink, '_blank');
 }
