561042f8b2dfe95ba2e4bd36f820dd11e967efc0
Tabaaza <omar.tabaza@gmail.com>
2024-06-04 10:52:22 +0200

    Button wasn't working, id os message box was changed without updating JS file

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function generateLink() {
   const countryCode = document.getElementById('country-code').value;
   let phone = document.getElementById('phone').value;
-  const message = document.getElementById('message').value;
+  const message = document.getElementById('messageMain').value;
   phone = phone.replace(/\D/g, '');
   const whatsappLink = `https://wa.me/${countryCode}${phone}?text=${encodeURIComponent(message)}`;
   window.open(whatsappLink, '_blank');
